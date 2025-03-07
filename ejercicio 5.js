3edae6ff2f7de34a36e737a50e4f8d4aa2cd474b
Yohan <yohanospina64@gmail.com>
2025-03-07 14:09:45 -0500

    Agregue la opcion de eliminar elementos en las distintas listas disponibles para que el usuario se pueda retractar de haber agregado al carrito de compras algun producto

--- ejercicio 5.js
+++ ejercicio 5.js
@@ -12,6 +12,7 @@ while (elejir_productos == true) {
     console.log("¿Deseas agragar un producto al carrito de compras?");
     console.log("1. Si");
     console.log("2. No");
+    console.log("3. Eliminar algun producto")
     eleccion_usuario = prompt();
 
     if (eleccion_usuario == 1) {
@@ -40,7 +41,7 @@ while (elejir_productos == true) {
         }
         
         console.log("------------------------------");
-        console.log("¿Deseas agragar mas productos?");
+        console.log("¿Deseas agragar mas productos o eliminar?");
         console.log("1. Si");
         console.log("2. No");
         let eleccion_usuario_seguir_elijiendo = prompt();
@@ -49,6 +50,66 @@ while (elejir_productos == true) {
             elejir_productos = true;
         } else {
             elejir_productos = false;
+        }
+    } else if (eleccion_usuario == 3) {
+        console.log("¿Que producto deseas aliminar?");
+        console.log(`1. Frutas: ${array_frutas}`);
+        console.log(`2. Lacteos: ${array_lacteos}`);
+        console.log(`3. Congelados: ${array_congelados}`);
+        console.log(`4. Dulces: ${array_dulces}`);
+        let eleccion_usuario_eliminar = prompt();
+        
+        if (eleccion_usuario_eliminar == 1) {
+            console.log("¿Que producto deseas eliminar?");
+            let numero_usuario_eliminar_producto = prompt("Escribe el numero del producto: ");
+            console.log(`Tu producto es ${array_frutas[Number(numero_usuario_eliminar_producto) - 1]} ?`);
+            console.log(`¿Deseas eliminar el producto ${array_frutas[Number(numero_usuario_eliminar_producto) - 1]}`);
+            console.log("1. Si");
+            console.log("2. No");
+            eleccion_usuario_eliminar = prompt();
+            
+            if (eleccion_usuario_eliminar == 1) {
+                array_frutas.splice(Number(numero_usuario_eliminar_producto) - 1, 1);
+            }
+        }
+        else if (eleccion_usuario_eliminar == 2) {
+            console.log("¿Que producto deseas eliminar?");
+            let numero_usuario_eliminar_producto = prompt("Escribe el numero del producto: ");
+            console.log(`Tu producto es ${array_lacteos[Number(numero_usuario_eliminar_producto) - 1]} ?`);
+            console.log(`¿Deseas eliminar el producto ${array_lacteos[Number(numero_usuario_eliminar_producto) - 1]}`);
+            console.log("1. Si");
+            console.log("2. No");
+            eleccion_usuario_eliminar = prompt();
+            
+            if (eleccion_usuario_eliminar == 1) {
+                array_lacteos.splice(Number(numero_usuario_eliminar_producto) - 1, 1);
+            }
+        }
+        else if (eleccion_usuario_eliminar == 3) {
+            console.log("¿Que producto deseas eliminar?");
+            let numero_usuario_eliminar_producto = prompt("Escribe el numero del producto: ");
+            console.log(`Tu producto es ${array_congelados[Number(numero_usuario_eliminar_producto) - 1]} ?`);
+            console.log(`¿Deseas eliminar el producto ${array_congelados[Number(numero_usuario_eliminar_producto) - 1]}`);
+            console.log("1. Si");
+            console.log("2. No");
+            eleccion_usuario_eliminar = prompt();
+            
+            if (eleccion_usuario_eliminar == 1) {
+                array_congelados.splice(Number(numero_usuario_eliminar_producto) - 1, 1);
+            }
+        }
+        else if (eleccion_usuario_eliminar == 4) {
+            console.log("¿Que producto deseas eliminar?");
+            let numero_usuario_eliminar_producto = prompt("Escribe el numero del producto: ");
+            console.log(`Tu producto es ${array_dulces[Number(numero_usuario_eliminar_producto) - 1]} ?`);
+            console.log(`¿Deseas eliminar el producto ${array_dulces[Number(numero_usuario_eliminar_producto) - 1]}`);
+            console.log("1. Si");
+            console.log("2. No");
+            eleccion_usuario_eliminar = prompt();
+            
+            if (eleccion_usuario_eliminar == 1) {
+                array_dulces.splice(Number(numero_usuario_eliminar_producto) - 1, 1);
+            }
         }
     } else {
         break;
